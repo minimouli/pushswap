ddb035cd35ac1407e1f81784b9d7091f5898641e
Adrien Belminksy <belminksy@gmail.com>
2021-12-11 16:00:50 +0100

    build: setup jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use-strict';
+
+module.exports = {
+    testMatch: [
+        '**/__tests__/*.spec.ts'
+    ],
+    testPathIgnorePatterns: [
+        '/node_modules/'
+    ]
+}
