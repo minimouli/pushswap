f294917c3314384ca578fc6abb5676e1d6b31079
Adrien Belminksy <belminksy@gmail.com>
2021-12-15 12:28:32 +0100

    feat: add new checks

--- src/objects/Interpreter.ts
+++ src/objects/Interpreter.ts
@@ -17,6 +17,8 @@ class Interpreter extends WritableStream {
     private list_a: List
     private list_b: List
 
+    private tokens: Token[] = []
+
     private hasInvalidOperation: boolean = false
     private invalidOperation: string = ''
 
@@ -37,6 +39,8 @@ class Interpreter extends WritableStream {
         // stop the execution if an invalid operation was found
         if (this.hasInvalidOperation)
             return
+
+        this.tokens = this.tokens.concat(tokens)
 
         let token: Token | undefined
 
@@ -102,6 +106,10 @@ class Interpreter extends WritableStream {
                 this.list_b.reverseRotate()
                 break
 
+            case TokenType.SPACE:
+            case TokenType.NEWLINE:
+                break
+
             default:
                 this.hasInvalidOperation = true
                 this.invalidOperation = token.content
@@ -130,6 +138,82 @@ class Interpreter extends WritableStream {
         throw new ExpectError(hint)
     }
 
+    checkHasOperations(): void {
+
+        if (this.tokens.length !== 0)
+            return
+
+        const hint: Hints.EqualHint = {
+            type: Hints.HintType.EQUAL,
+            status: Hints.HintStatus.FAILURE,
+            message: 'The output must contain at least one operation or a new line at the end.',
+            received: {
+                value: '""',
+                type: Hints.ObjectType.STRING
+            },
+            expected: {
+                value: 'sa | sb | sc | pa | pb | ra | rb | rr | rra | rrb | rrr',
+                type: Hints.ObjectType.STRING
+            }
+        }
+
+        throw new ExpectError(hint)
+    }
+
+    checkHasNoExtraSpaces(): void {
+
+        if (this.tokens.length === 0)
+            return
+
+        const hint: Hints.EqualHint = {
+            type: Hints.HintType.EQUAL,
+            status: Hints.HintStatus.FAILURE,
+            message: 'The output must not contain extra spaces.',
+            received: {
+                value: '" "',
+                type: Hints.ObjectType.STRING
+            },
+            expected: {
+                value: 'sa | sb | sc | pa | pb | ra | rb | rr | rra | rrb | rrr',
+                type: Hints.ObjectType.STRING
+            }
+        }
+
+        if (this.tokens[0].type === TokenType.SPACE)
+            throw new ExpectError(hint)
+
+        this.tokens.reduce((previous: Token, current: Token) => {
+
+            if (previous.type === TokenType.SPACE && current.type === TokenType.SPACE)
+                throw new ExpectError(hint)
+
+            return current
+        })
+    }
+
+    checkHasANewLine(): void {
+
+        if (this.tokens.length === 0)
+            return
+
+        const hint: Hints.EqualHint = {
+            type: Hints.HintType.EQUAL,
+            status: Hints.HintStatus.FAILURE,
+            message: 'The output must have a new line at the end.',
+            received: {
+                value: '""',
+                type: Hints.ObjectType.STRING
+            },
+            expected: {
+                value: '\\n',
+                type: Hints.ObjectType.STRING
+            }
+        }
+
+        if (this.tokens[this.tokens.length - 1].type !== TokenType.NEWLINE)
+            throw new ExpectError(hint)
+    }
+
     checkFirstToBeSorted(): void {
 
         if (this.list_a.length === 0)
@@ -151,7 +235,7 @@ class Interpreter extends WritableStream {
         const hint: Hints.EqualHint = {
             type: Hints.HintType.EQUAL,
             status: Hints.HintStatus.FAILURE,
-            message: 'The second list should be empty.',
+            message: 'The second list must be empty.',
             received: {
                 value: '[...]',
                 type: Hints.ObjectType.OBJECT
